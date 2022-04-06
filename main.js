598f4e2c6cfe016f4e8ea5bf708dfa2881e9ade0
Abhiuday17 <79649068+Abhiuday17@users.noreply.github.com>
2022-04-06 13:09:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -8,3 +8,68 @@ timer_check = "";
 drawn_sketch = "";
 answer_holder = "";
 score = 0;
+sketch = ""
+
+function updateCanvas(){
+    background("white")
+    random_no = Math.floor((Math.random()*quick_draw_data_set.length)+1);
+    sketch =  quick_draw_data_set[random_no];
+    document.getElementById("sketch_to_be_drawn").innerHTML = "Sketch To Be Drawn: "+sketch;
+
+}
+
+function setup(){
+    canvas = createCanvas(280 , 280 )
+    canvas.center()
+    background("white")
+    canvas.mouseReleased(classifyCanvas)
+}
+
+function draw(){
+    
+    strokeWeight(13)
+    stroke(0)
+    if(mouseIsPressed){
+        line(pmouseX , pmouseY , mouseX , mouseY)
+    }
+    checkSketch()
+    if(drawn_sketch == sketch){
+        answer_holder = "set"
+        score = score + 1
+        document.getElementById("score").innerHTML = "score : " + score 
+    }
+}
+
+function preload(){
+    classifier = ml5.imageClassifier("DoodleNet")
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas , gotResult)
+}
+
+function gotResult(error , results){
+    if(error){
+        console.error(error)
+    }
+    else{
+        console.log(results)
+        drawn_sketch = results[0].label
+        document.getElementById("your_sketch").innerHTML = "Your Sketch :" + drawn_sketch
+        document.getElementById("confidence").innerHTML = "Confidence :" + Math.round(results[0].confidence*100)+ "%"
+    }
+}
+
+function checkSketch(){
+    timer_counter++
+    document.getElementById("timer").innerHTML = "timer :" + timer_counter
+    if(timer_counter>400){
+        timer_counter = 0 
+        timer_check = "completed"
+    }
+    if(timer_check == "completed" || answer_holder  == "set"){
+        timer_check = "" 
+        answer_holder = ""
+        updateCanvas()
+    }
+}
